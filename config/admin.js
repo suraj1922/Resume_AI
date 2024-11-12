9ca2ee74e61da1b01ac8d1429ed7dd57f4caeab1
SurajGupta <surajgupta19224@gmail.com>
2024-11-12 20:42:55 +0530

    .

--- config/admin.js
+++ config/admin.js
@@ -5,11 +5,7 @@ module.exports = ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
+  
   flags: {
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
